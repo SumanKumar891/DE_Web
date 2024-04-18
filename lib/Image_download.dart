617da201fac745c28f374641974564c00eeeb459
Vanshika <103096079+SumanKumar891@users.noreply.github.com>
2024-04-18 12:02:39 +0530

    downloaderimg

--- lib/Image_download.dart
+++ lib/Image_download.dart
@@ -432,25 +432,291 @@
 //   }
 // }
 
+// import 'package:flutter/material.dart';
+// import 'dart:convert';
+// import 'package:http/http.dart' as http;
+// import 'package:intl/intl.dart';
+// import 'dart:io';
+// import 'package:path_provider/path_provider.dart';
+// import 'package:fluttertoast/fluttertoast.dart';
+
+// class Pictures extends StatefulWidget {
+//   final String deviceId;
+//   const Pictures({super.key, required this.deviceId});
+//   @override
+//   _PicturesState createState() => _PicturesState();
+// }
+
+// class _PicturesState extends State<Pictures> {
+//   // final String apiUrl =
+//   //     'https://tulp6xq61c.execute-api.us-east-1.amazonaws.com/dep/images?device=02&date=23-02-2024';
+
+//   List<String> imageUrls = [];
+//   int batchSize = 10;
+//   int currentPage = 0;
+//   DateTime selectedDate = DateTime.now();
+
+//   bool isLoading = true;
+
+//   var _startDate;
+
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//       appBar: AppBar(
+//         title: Text(
+//           'Image Data for ' + widget.deviceId,
+//           style: TextStyle(
+//             fontSize: 20.0,
+//             letterSpacing: 1.0,
+//             color: Colors.white,
+//           ),
+//         ),
+//         // actions: [
+//         //   PopupMenuButton<String>(
+//         //     onSelected: (value) => onDownloadOptionSelected(value),
+//         //     itemBuilder: (BuildContext context) {
+//         //       return ['Download Current Batch', 'Download All Images']
+//         //           .map((String choice) {
+//         //         return PopupMenuItem<String>(
+//         //           value: choice,
+//         //           child: Text(choice),
+//         //         );
+//         //       }).toList();
+//         //     },
+//         //   ),
+//         // ],
+//         backgroundColor: Colors.green,
+//         elevation: 0.0,
+//         centerTitle: true,
+//       ),
+//       body: Container(
+//         padding: EdgeInsets.all(16.0),
+//         child: Column(
+//           children: [
+//             TextFormField(
+//               onTap: () => _selectDate(context),
+//               decoration: InputDecoration(
+//                 labelText: 'Select Date',
+//                 border: OutlineInputBorder(),
+//                 contentPadding:
+//                     EdgeInsets.symmetric(vertical: 12, horizontal: 16),
+//               ),
+//               controller: TextEditingController(
+//                   text: selectedDate != null
+//                       ? DateFormat('dd-MM-yyyy').format(selectedDate)
+//                       : ''),
+//             ),
+//             Expanded(
+//               child: isLoading
+//                   ? FutureBuilder<List<String>>(
+//                       future: fetchImages(widget.deviceId, selectedDate),
+//                       builder: (context, snapshot) {
+//                         if (snapshot.hasError) {
+//                           return Text('Error: ${snapshot.error}');
+//                         } else if (!snapshot.hasData ||
+//                             snapshot.data!.isEmpty) {
+//                           return Text('Loading...');
+//                         } else {
+//                           return ListView.builder(
+//                             itemCount: snapshot.data!.length,
+//                             itemBuilder: (context, index) {
+//                               return Image.network(snapshot.data![index]);
+//                             },
+//                           );
+//                         }
+//                       },
+//                     )
+//                   : ListView.builder(
+//                       itemCount: imageUrls.length,
+//                       itemBuilder: (context, index) {
+//                         return Image.network(imageUrls[index]);
+//                       },
+//                     ),
+//             ),
+//             Row(
+//               mainAxisAlignment: MainAxisAlignment.center,
+//               children: [
+//                 ElevatedButton(
+//                   style: ElevatedButton.styleFrom(primary: Colors.green),
+//                   onPressed: currentPage > 0 ? moveToPreviousPage : null,
+//                   child: Text('Previous'),
+//                 ),
+//                 SizedBox(width: 16),
+//                 ElevatedButton(
+//                   style: ElevatedButton.styleFrom(primary: Colors.green),
+//                   onPressed: moveToNextPage,
+//                   child: Text('Next'),
+//                 ),
+//               ],
+//             ),
+//           ],
+//         ),
+//       ),
+//     );
+//   }
+
+//   Future<void> _selectDate(BuildContext context) async {
+//     final DateTime? picked = await showDatePicker(
+//       context: context,
+//       initialDate: selectedDate ?? DateTime.now(),
+//       firstDate: DateTime(1990),
+//       lastDate: DateTime.now(),
+//       builder: (context, child) {
+//         return Theme(
+//           data: Theme.of(context).copyWith(
+//             colorScheme: const ColorScheme.light(
+//               primary: Colors.green,
+//               onPrimary: Colors.white,
+//               onSurface: Colors.purple,
+//             ),
+//             textButtonTheme: TextButtonThemeData(
+//               style: TextButton.styleFrom(
+//                 elevation: 10,
+//                 backgroundColor: Colors.black, // button text color
+//               ),
+//             ),
+//           ),
+//           // child: child!,
+//           child: MediaQuery(
+//             data: MediaQuery.of(context).copyWith(alwaysUse24HourFormat: true),
+//             child: child ?? Container(),
+//           ),
+//         );
+//       },
+//     );
+
+//     if (picked != null && picked != selectedDate) {
+//       final newImages = await fetchImages(widget.deviceId, picked);
+//       setState(() {
+//         selectedDate = picked;
+//         imageUrls = newImages;
+//       });
+//     }
+//   }
+
+//   Future<List<String>> fetchImages(String device, DateTime date) async {
+//     final formattedDate = DateFormat('dd-MM-yyyy').format(date);
+//     final response = await http.get(Uri.parse(
+//         'https://tulp6xq61c.execute-api.us-east-1.amazonaws.com/dep/images?device=$device&date=${formattedDate.toString()}'));
+//     List<String> currentBatch = [];
+
+//     if (response.statusCode == 200) {
+//       final bodyJson = json.decode(response.body);
+//       final images = json.decode(bodyJson['body'])['images'];
+
+//       // Extract image URLs
+//       for (int i = currentPage * batchSize;
+//           i < (currentPage + 1) * batchSize && i < images.length;
+//           i++) {
+//         currentBatch.add(images[i]);
+//       }
+
+//       // Update the list of image URLs
+//       imageUrls = currentBatch;
+//     } else {
+//       throw Exception('Failed to load images');
+//     }
+
+//     // Show loading text for a short duration
+//     Future.delayed(Duration(milliseconds: 500), () {
+//       setState(() {
+//         isLoading = false;
+//       });
+//     });
+
+//     return imageUrls;
+//   }
+
+//   void moveToNextPage() {
+//     setState(() {
+//       currentPage++;
+//       isLoading = true;
+//     });
+//   }
+
+//   void moveToPreviousPage() {
+//     setState(() {
+//       currentPage--;
+//       isLoading = true;
+//     });
+//   }
+
+//   // Future<void> onDownloadOptionSelected(String option) async {
+//   //   if (option == 'Downlaod Current Batch') {
+//   //     await downloadCurrentBatch();
+//   //   } else if (option == ' Downlaod All Images') {
+//   //     await downloadAllImages();
+//   //   }
+//   // }
+
+//   // Future<void> downloadCurrentBatch() async {
+//   //   for (String imageUrl in imageUrls) {
+//   //     await _downloadImage(imageUrl);
+//   //   }
+//   // }
+
+//   // Future<void> downloadAllImages() async {
+//   //   final response = await http.get(Uri.parse(apiUrl));
+//   //   if (response.statusCode == 200) {
+//   //     final bodyJson = json.decode(response.body);
+//   //     final images = json.decode(bodyJson['body'])['images'];
+
+//   //     for (String imageUrl in images) {
+//   //       await _downloadImage(imageUrl);
+//   //     }
+//   //   } else {
+//   //     throw Exception('Failed to load images');
+//   //   }
+//   // }
+
+//   // Future<void> _downloadImage(String imageUrl) async {
+//   //   WidgetsFlutterBinding.ensureInitialized();
+
+//   //   final response = await http.get(Uri.parse(imageUrl));
+
+//   //   if (response.statusCode == 200) {
+//   //     final appDocumentsDirectory = await getApplicationDocumentsDirectory();
+//   //     final filePath =
+//   //         '${appDocumentsDirectory.path}/${imageUrl.split('/').last}';
+
+//   //     File file = File(filePath);
+//   //     await file.writeAsBytes(response.bodyBytes);
+
+//   //     // Display a toast or any other message to indicate the download success
+//   //     Fluttertoast.showToast(msg: 'Downloaded: $filePath');
+//   //   } else {
+//   //     throw Exception('Failed to download image: $imageUrl');
+//   //   }
+//   // }
+// }
+
+import 'dart:convert';
 import 'package:flutter/material.dart';
 import 'package:fluttertoast/fluttertoast.dart';
+import 'package:intl/intl.dart';
+import 'dart:html' as html;
+import 'package:http/http.dart' as http;
 
 class Pictures extends StatefulWidget {
   final String deviceId;
-  const Pictures({super.key, required this.deviceId});
+
+  const Pictures({Key? key, required this.deviceId}) : super(key: key);
+
   @override
   _PicturesState createState() => _PicturesState();
 }
 
 class _PicturesState extends State<Pictures> {
   List<String> imageUrls = [];
-  int batchSize = 10;
-  int currentPage = 0;
-  DateTime selectedDate = DateTime.now();
+  DateTime? _startDate; // Track selected start date
 
-  bool isLoading = true;
-
-  var _startDate;
+  @override
+  void initState() {
+    super.initState();
+    _startDate = DateTime.now();
+    fetchImages(widget.deviceId, _startDate!); // Fetch images initially
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -471,62 +737,50 @@ class _PicturesState extends State<Pictures> {
       body: Container(
         padding: EdgeInsets.all(16.0),
         child: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
           children: [
-            TextFormField(
-              onTap: () => _selectDate(context),
-              decoration: InputDecoration(
-                labelText: 'Select Date',
-                border: OutlineInputBorder(),
-                contentPadding:
-                    EdgeInsets.symmetric(vertical: 12, horizontal: 16),
-              ),
-              controller: TextEditingController(
-                  text: selectedDate != null
-                      ? DateFormat('dd-MM-yyyy').format(selectedDate)
-                      : ''),
-            ),
-            Expanded(
-              child: isLoading
-                  ? FutureBuilder<List<String>>(
-                      future: fetchImages(widget.deviceId, selectedDate),
-                      builder: (context, snapshot) {
-                        if (snapshot.hasError) {
-                          return Text('Error: ${snapshot.error}');
-                        } else if (!snapshot.hasData ||
-                            snapshot.data!.isEmpty) {
-                          return Text('Loading...');
-                        } else {
-                          return ListView.builder(
-                            itemCount: snapshot.data!.length,
-                            itemBuilder: (context, index) {
-                              return Image.network(snapshot.data![index]);
-                            },
-                          );
-                        }
-                      },
-                    )
-                  : ListView.builder(
-                      itemCount: imageUrls.length,
-                      itemBuilder: (context, index) {
-                        return Image.network(imageUrls[index]);
-                      },
-                    ),
-            ),
             Row(
-              mainAxisAlignment: MainAxisAlignment.center,
               children: [
-                ElevatedButton(
-                  style: ElevatedButton.styleFrom(primary: Colors.green),
-                  onPressed: currentPage > 0 ? moveToPreviousPage : null,
-                  child: Text('Previous'),
+                Expanded(
+                  child: TextFormField(
+                    readOnly: true,
+                    decoration: InputDecoration(
+                      labelText: 'Start Date',
+                      border: OutlineInputBorder(),
+                      contentPadding:
+                          EdgeInsets.symmetric(vertical: 12, horizontal: 16),
+                    ),
+                    controller: TextEditingController(
+                      text: _startDate != null
+                          ? DateFormat('yyyy-MM-dd').format(_startDate!)
+                          : '',
+                    ),
+                    onTap: () => _selectDate(
+                        context), // Use _selectDate for date field tap
+                  ),
                 ),
-                SizedBox(width: 16),
+                SizedBox(
+                    width: 16.0), // Add spacing between date field and button
                 ElevatedButton(
-                  style: ElevatedButton.styleFrom(primary: Colors.green),
-                  onPressed: moveToNextPage,
-                  child: Text('Next'),
+                  onPressed: () => _downloadAllImages(),
+                  style: ElevatedButton.styleFrom(
+                    primary: Colors
+                        .green, // Set the button's background color to green
+                  ),
+                  child: Text('Links for Images'),
                 ),
               ],
+            ),
+            SizedBox(height: 16.0), // Add spacing between button and image list
+            Expanded(
+              child: ListView.builder(
+                itemCount: imageUrls.length,
+                itemBuilder: (context, index) {
+                  return ListTile(
+                    title: Image.network(imageUrls[index]),
+                  );
+                },
+              ),
             ),
           ],
         ),
@@ -537,112 +791,51 @@ class _PicturesState extends State<Pictures> {
   Future<void> _selectDate(BuildContext context) async {
     final DateTime? picked = await showDatePicker(
       context: context,
-      initialDate: selectedDate ?? DateTime.now(),
+      initialDate: _startDate ?? DateTime.now(),
       firstDate: DateTime(1990),
       lastDate: DateTime.now(),
     );
 
-    if (picked != null && picked != selectedDate) {
-      final newImages = await fetchImages(widget.deviceId, picked);
+    if (picked != null && picked != _startDate) {
       setState(() {
-        selectedDate = picked;
-        imageUrls = newImages;
+        _startDate = picked;
       });
+      fetchImages(
+          widget.deviceId, _startDate!); // Fetch images on date selection
     }
   }
 
-  Future<List<String>> fetchImages(String device, DateTime date) async {
+  Future<void> fetchImages(String device, DateTime date) async {
     final formattedDate = DateFormat('dd-MM-yyyy').format(date);
-    final response = await http.get(Uri.parse(
-        'https://tulp6xq61c.execute-api.us-east-1.amazonaws.com/dep/images?device=$device&date=${formattedDate.toString()}'));
-    List<String> currentBatch = [];
+    try {
+      final response = await http.get(Uri.parse(
+          'https://tulp6xq61c.execute-api.us-east-1.amazonaws.com/dep/images?device=$device&date=$formattedDate'));
 
-    if (response.statusCode == 200) {
-      final bodyJson = json.decode(response.body);
-      final images = json.decode(bodyJson['body'])['images'];
-
-      // Extract image URLs
-      for (int i = currentPage * batchSize;
-          i < (currentPage + 1) * batchSize && i < images.length;
-          i++) {
-        currentBatch.add(images[i]);
+      if (response.statusCode == 200) {
+        final bodyJson = json.decode(response.body);
+        final images = json.decode(bodyJson['body'])['images'];
+        setState(() {
+          imageUrls = List<String>.from(images);
+        });
+      } else {
+        throw Exception('Failed to load images');
       }
-
-      // Update the list of image URLs
-      imageUrls = currentBatch;
-    } else {
-      throw Exception('Failed to load images');
+    } catch (e) {
+      print('Error fetching images: $e');
+      Fluttertoast.showToast(
+        msg: 'Error fetching images',
+        toastLength: Toast.LENGTH_LONG,
+      );
     }
-
-    // Show loading text for a short duration
-    Future.delayed(Duration(milliseconds: 500), () {
-      setState(() {
-        isLoading = false;
-      });
-    });
-
-    return imageUrls;
   }
 
-  void moveToNextPage() {
-    setState(() {
-      currentPage++;
-      isLoading = true;
-    });
+  void _downloadAllImages() {
+    String concatenatedUrls = imageUrls.join('\n');
+    String blobUrl = 'data:application/octet-stream;charset=utf-16le;base64,' +
+        base64Encode(utf8.encode(concatenatedUrls));
+
+    html.AnchorElement anchorElement = html.AnchorElement(href: blobUrl)
+      ..setAttribute('download', 'images.txt');
+    anchorElement.click();
   }
-
-  void moveToPreviousPage() {
-    setState(() {
-      currentPage--;
-      isLoading = true;
-    });
-  }
-
-  // Future<void> onDownloadOptionSelected(String option) async {
-  //   if (option == 'Downlaod Current Batch') {
-  //     await downloadCurrentBatch();
-  //   } else if (option == ' Downlaod All Images') {
-  //     await downloadAllImages();
-  //   }
-  // }
-
-  // Future<void> downloadCurrentBatch() async {
-  //   for (String imageUrl in imageUrls) {
-  //     await _downloadImage(imageUrl);
-  //   }
-  // }
-
-  // Future<void> downloadAllImages() async {
-  //   final response = await http.get(Uri.parse(apiUrl));
-  //   if (response.statusCode == 200) {
-  //     final bodyJson = json.decode(response.body);
-  //     final images = json.decode(bodyJson['body'])['images'];
-
-  //     for (String imageUrl in images) {
-  //       await _downloadImage(imageUrl);
-  //     }
-  //   } else {
-  //     throw Exception('Failed to load images');
-  //   }
-  // }
-
-  // Future<void> _downloadImage(String imageUrl) async {
-  //   WidgetsFlutterBinding.ensureInitialized();
-
-  //   final response = await http.get(Uri.parse(imageUrl));
-
-  //   if (response.statusCode == 200) {
-  //     final appDocumentsDirectory = await getApplicationDocumentsDirectory();
-  //     final filePath =
-  //         '${appDocumentsDirectory.path}/${imageUrl.split('/').last}';
-
-  //     File file = File(filePath);
-  //     await file.writeAsBytes(response.bodyBytes);
-
-//   //     // Display a toast or any other message to indicate the download success
-//   //     Fluttertoast.showToast(msg: 'Downloaded: $filePath');
-//   //   } else {
-//   //     throw Exception('Failed to download image: $imageUrl');
-//   //   }
-//   // }
-// }
+}
