e578147a20d0a424046c57f0656deec79fe283b6
gyan3000 <gyaneshwar.knp.2019@gmail.com>
2024-06-20 08:26:53 +0000

    Updated build/web

--- build/web/flutter_service_worker.js
+++ build/web/flutter_service_worker.js
@@ -28,8 +28,8 @@ const RESOURCES = {
 "assets/FontManifest.json": "dc3d03800ccca4601324923c0b1d6d57",
 "assets/fonts/MaterialIcons-Regular.otf": "e7069dfd19b331be16bed984668fe080",
 "assets/AssetManifest.json": "d6337fe45cdb0494807df111ce952bbd",
-"index.html": "2d11445defe226e93a1e42c114942399",
-"/": "2d11445defe226e93a1e42c114942399",
+"index.html": "506c1e0106bbe8bfec2815bdcec4ad04",
+"/": "506c1e0106bbe8bfec2815bdcec4ad04",
 "favicon1.png": "5898be9902795cbc619b5ff1dbb78287",
 "favicon2.png": "b15589c6eb6a8422d67e7c8de28a7833",
 "manifest.json": "1db7e88037c7a17cb8e31665b0415e4d"
