f240605de77a37162835195d76ae7f0bea846874
Vanshika <103096079+SumanKumar891@users.noreply.github.com>
2024-07-10 16:31:18 +0530

    revversed

--- lib/Image_download.dart
+++ lib/Image_download.dart
@@ -1,185 +1,3 @@
-// import 'dart:convert';
-// import 'package:flutter/material.dart';
-// import 'package:fluttertoast/fluttertoast.dart';
-// import 'package:intl/intl.dart';
-// import 'dart:html' as html;
-// import 'package:http/http.dart' as http;
-
-// class Pictures extends StatefulWidget {
-//   final String deviceId;
-
-//   const Pictures({Key? key, required this.deviceId}) : super(key: key);
-
-//   @override
-//   _PicturesState createState() => _PicturesState();
-// }
-
-// class _PicturesState extends State<Pictures> {
-//   List<String> imageUrls = [];
-//   DateTime? _startDate; // Track selected start date
-
-//   @override
-//   void initState() {
-//     super.initState();
-//     _startDate = DateTime.now();
-//     fetchImages(widget.deviceId, _startDate!); // Fetch images initially
-//   }
-
-//   Future<void> _refreshData() async {
-//     // Fetch updated images based on the current start date
-//     await fetchImages(widget.deviceId, _startDate!);
-//   }
-
-//   @override
-//   Widget build(BuildContext context) {
-//     return Scaffold(
-//       appBar: AppBar(
-//         title: Text(
-//           'Image Data for ' + widget.deviceId,
-//           style: TextStyle(
-//             fontSize: 20.0,
-//             letterSpacing: 1.0,
-//             color: Colors.white,
-//           ),
-//         ),
-//         backgroundColor: Colors.brown[400],
-//         elevation: 0.0,
-//         centerTitle: true,
-//       ),
-//       body: Container(
-//         padding: EdgeInsets.all(16.0),
-//         child: Column(
-//           crossAxisAlignment: CrossAxisAlignment.stretch,
-//           children: [
-//             Row(
-//               mainAxisAlignment: MainAxisAlignment.spaceBetween,
-//               children: [
-//                 Expanded(
-//                   child: TextFormField(
-//                     readOnly: true,
-//                     decoration: InputDecoration(
-//                       labelText: 'Select Date',
-//                       border: OutlineInputBorder(),
-//                       contentPadding:
-//                           EdgeInsets.symmetric(vertical: 12, horizontal: 16),
-//                     ),
-//                     controller: TextEditingController(
-//                       text: _startDate != null
-//                           ? DateFormat('yyyy-MM-dd').format(_startDate!)
-//                           : '',
-//                     ),
-//                     onTap: () => _selectDate(context),
-//                   ),
-//                 ),
-//                 SizedBox(
-//                     width: 16.0), // Add spacing between date field and buttons
-//                 ElevatedButton(
-//                   onPressed: () => _downloadAllImages(),
-//                   style: ElevatedButton.styleFrom(
-//                     primary: Colors.brown[600],
-//                     // Set button's background color to green
-//                   ),
-//                   child: Text('Links for Images'),
-//                 ),
-//                 SizedBox(width: 16.0), // Add spacing between buttons
-//                 ElevatedButton.icon(
-//                   onPressed: _refreshData,
-//                   style: ElevatedButton.styleFrom(
-//                     primary: Colors
-//                         .brown[600], // Set button's background color to blue
-//                   ),
-//                   icon: Icon(Icons.refresh), // Add refresh icon
-//                   label: Text('Refresh'),
-//                 ),
-//               ],
-//             ),
-//             SizedBox(height: 16.0), // Add spacing before text and image list
-//             Text(
-//               'Total Images: ${imageUrls.length}',
-//               style: TextStyle(fontSize: 16.0, fontWeight: FontWeight.bold),
-//             ),
-//             SizedBox(height: 8.0), // Add spacing between text and image list
-//             Expanded(
-//               child: ListView.builder(
-//                 itemCount: imageUrls.length,
-//                 itemBuilder: (context, index) {
-//                   // Calculate the reversed index
-//                   int reversedIndex = imageUrls.length - 1 - index;
-//                   return ListTile(
-//                     title: Image.network(imageUrls[reversedIndex]),
-//                   );
-//                 },
-//               ),
-//             ),
-//           ],
-//         ),
-//       ),
-//     );
-//   }
-
-//   Future<void> _selectDate(BuildContext context) async {
-//     final DateTime? picked = await showDatePicker(
-//       context: context,
-//       initialDate: _startDate ?? DateTime.now(),
-//       firstDate: DateTime(1990),
-//       lastDate: DateTime.now(),
-//     );
-
-//     if (picked != null && picked != _startDate) {
-//       setState(() {
-//         _startDate = picked;
-//       });
-//       await fetchImages(
-//           widget.deviceId, _startDate!); // Fetch images on date selection
-//     }
-//   }
-
-//   Future<void> fetchImages(String device, DateTime date) async {
-//     final formattedDate = DateFormat('dd-MM-yyyy').format(date);
-//     try {
-//       final response = await http.get(Uri.parse(
-//           'https://tulp6xq61c.execute-api.us-east-1.amazonaws.com/dep/images?device=$device&date=$formattedDate'));
-
-//       if (response.statusCode == 200) {
-//         final bodyJson = json.decode(response.body);
-//         final images = json.decode(bodyJson['body'])['images'];
-//         setState(() {
-//           imageUrls = List<String>.from(images);
-//         });
-//       } else {
-//         throw Exception('Failed to load images');
-//       }
-//     } catch (e) {
-//       print('Error fetching images: $e');
-//       Fluttertoast.showToast(
-//         msg: 'Error fetching images',
-//         toastLength: Toast.LENGTH_LONG,
-//       );
-//     }
-//   }
-
-//   void _downloadAllImages() {
-//     // Generate text file content with total count and image URLs
-//     int totalCount = imageUrls.length;
-//     String concatenatedUrls = imageUrls.join('\n');
-//     String fileContent = 'Total Images: $totalCount\n$concatenatedUrls';
-
-//     // Encode content as UTF-8
-//     List<int> encodedContent = utf8.encode(fileContent);
-
-//     // Create blob URL for download
-//     String blobUrl = 'data:application/octet-stream;charset=utf-8;base64,' +
-//         base64Encode(encodedContent);
-
-//     // Create anchor element for download
-//     html.AnchorElement anchorElement = html.AnchorElement(href: blobUrl)
-//       ..setAttribute('download', 'images.txt');
-
-//     // Trigger download
-//     anchorElement.click();
-//   }
-// }
-
 import 'dart:convert';
 import 'package:flutter/material.dart';
 import 'package:fluttertoast/fluttertoast.dart';
@@ -197,8 +15,7 @@ class Pictures extends StatefulWidget {
 }
 
 class _PicturesState extends State<Pictures> {
-  List<Map<String, String>> imageUrls =
-      []; // Change to hold URLs and timestamps
+  List<String> imageUrls = [];
   DateTime? _startDate; // Track selected start date
 
   @override
@@ -288,22 +105,8 @@ class _PicturesState extends State<Pictures> {
                 itemBuilder: (context, index) {
                   // Calculate the reversed index
                   int reversedIndex = imageUrls.length - 1 - index;
-                  final image = imageUrls[reversedIndex];
                   return ListTile(
-                    title: Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        Image.network(image['url']!), // Display the image
-                        SizedBox(
-                            height:
-                                8.0), // Add spacing between image and timestamp
-                        Text(
-                          image['timestamp']!,
-                          style: TextStyle(
-                              fontSize: 14.0, color: Colors.grey[600]),
-                        ), // Display the timestamp
-                      ],
-                    ),
+                    title: Image.network(imageUrls[reversedIndex]),
                   );
                 },
               ),
@@ -339,20 +142,9 @@ class _PicturesState extends State<Pictures> {
 
       if (response.statusCode == 200) {
         final bodyJson = json.decode(response.body);
-        final imagesData =
-            json.decode(bodyJson['body'])['images'] as List<dynamic>;
-
-        // Create a list to hold the image URL and timestamp pairs
-        List<Map<String, String>> imageList = [];
-        for (var imageData in imagesData) {
-          // Extract URL and timestamp
-          String url = imageData['url'];
-          String timestamp = imageData['url'].split('/')[3];
-          imageList.add({'url': url, 'timestamp': timestamp});
-        }
-
+        final images = json.decode(bodyJson['body'])['images'];
         setState(() {
-          imageUrls = imageList;
+          imageUrls = List<String>.from(images);
         });
       } else {
         throw Exception('Failed to load images');
@@ -367,15 +159,10 @@ class _PicturesState extends State<Pictures> {
   }
 
   void _downloadAllImages() {
-    // Generate text file content with total count, image URLs, and timestamps
+    // Generate text file content with total count and image URLs
     int totalCount = imageUrls.length;
-    StringBuffer buffer = StringBuffer();
-    buffer.writeln('Total Images: $totalCount');
-    for (var image in imageUrls) {
-      buffer.writeln('${image['url']} | ${image['timestamp']}');
-    }
-
-    String fileContent = buffer.toString();
+    String concatenatedUrls = imageUrls.join('\n');
+    String fileContent = 'Total Images: $totalCount\n$concatenatedUrls';
 
     // Encode content as UTF-8
     List<int> encodedContent = utf8.encode(fileContent);
@@ -392,3 +179,216 @@ class _PicturesState extends State<Pictures> {
     anchorElement.click();
   }
 }
+
+// import 'dart:convert';
+// import 'package:flutter/material.dart';
+// import 'package:fluttertoast/fluttertoast.dart';
+// import 'package:intl/intl.dart';
+// import 'dart:html' as html;
+// import 'package:http/http.dart' as http;
+
+// class Pictures extends StatefulWidget {
+//   final String deviceId;
+
+//   const Pictures({Key? key, required this.deviceId}) : super(key: key);
+
+//   @override
+//   _PicturesState createState() => _PicturesState();
+// }
+
+// class _PicturesState extends State<Pictures> {
+//   List<Map<String, String>> imageUrls =
+//       []; // Change to hold URLs and timestamps
+//   DateTime? _startDate; // Track selected start date
+
+//   @override
+//   void initState() {
+//     super.initState();
+//     _startDate = DateTime.now();
+//     fetchImages(widget.deviceId, _startDate!); // Fetch images initially
+//   }
+
+//   Future<void> _refreshData() async {
+//     // Fetch updated images based on the current start date
+//     await fetchImages(widget.deviceId, _startDate!);
+//   }
+
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//       appBar: AppBar(
+//         title: Text(
+//           'Image Data for ' + widget.deviceId,
+//           style: TextStyle(
+//             fontSize: 20.0,
+//             letterSpacing: 1.0,
+//             color: Colors.white,
+//           ),
+//         ),
+//         backgroundColor: Colors.brown[400],
+//         elevation: 0.0,
+//         centerTitle: true,
+//       ),
+//       body: Container(
+//         padding: EdgeInsets.all(16.0),
+//         child: Column(
+//           crossAxisAlignment: CrossAxisAlignment.stretch,
+//           children: [
+//             Row(
+//               mainAxisAlignment: MainAxisAlignment.spaceBetween,
+//               children: [
+//                 Expanded(
+//                   child: TextFormField(
+//                     readOnly: true,
+//                     decoration: InputDecoration(
+//                       labelText: 'Select Date',
+//                       border: OutlineInputBorder(),
+//                       contentPadding:
+//                           EdgeInsets.symmetric(vertical: 12, horizontal: 16),
+//                     ),
+//                     controller: TextEditingController(
+//                       text: _startDate != null
+//                           ? DateFormat('yyyy-MM-dd').format(_startDate!)
+//                           : '',
+//                     ),
+//                     onTap: () => _selectDate(context),
+//                   ),
+//                 ),
+//                 SizedBox(
+//                     width: 16.0), // Add spacing between date field and buttons
+//                 ElevatedButton(
+//                   onPressed: () => _downloadAllImages(),
+//                   style: ElevatedButton.styleFrom(
+//                     primary: Colors.brown[600],
+//                     // Set button's background color to green
+//                   ),
+//                   child: Text('Links for Images'),
+//                 ),
+//                 SizedBox(width: 16.0), // Add spacing between buttons
+//                 ElevatedButton.icon(
+//                   onPressed: _refreshData,
+//                   style: ElevatedButton.styleFrom(
+//                     primary: Colors
+//                         .brown[600], // Set button's background color to blue
+//                   ),
+//                   icon: Icon(Icons.refresh), // Add refresh icon
+//                   label: Text('Refresh'),
+//                 ),
+//               ],
+//             ),
+//             SizedBox(height: 16.0), // Add spacing before text and image list
+//             Text(
+//               'Total Images: ${imageUrls.length}',
+//               style: TextStyle(fontSize: 16.0, fontWeight: FontWeight.bold),
+//             ),
+//             SizedBox(height: 8.0), // Add spacing between text and image list
+//             Expanded(
+//               child: ListView.builder(
+//                 itemCount: imageUrls.length,
+//                 itemBuilder: (context, index) {
+//                   // Calculate the reversed index
+//                   int reversedIndex = imageUrls.length - 1 - index;
+//                   final image = imageUrls[reversedIndex];
+//                   return ListTile(
+//                     title: Column(
+//                       crossAxisAlignment: CrossAxisAlignment.start,
+//                       children: [
+//                         Image.network(image['url']!), // Display the image
+//                         SizedBox(
+//                             height:
+//                                 8.0), // Add spacing between image and timestamp
+//                         Text(
+//                           image['timestamp']!,
+//                           style: TextStyle(
+//                               fontSize: 14.0, color: Colors.grey[600]),
+//                         ), // Display the timestamp
+//                       ],
+//                     ),
+//                   );
+//                 },
+//               ),
+//             ),
+//           ],
+//         ),
+//       ),
+//     );
+//   }
+
+//   Future<void> _selectDate(BuildContext context) async {
+//     final DateTime? picked = await showDatePicker(
+//       context: context,
+//       initialDate: _startDate ?? DateTime.now(),
+//       firstDate: DateTime(1990),
+//       lastDate: DateTime.now(),
+//     );
+
+//     if (picked != null && picked != _startDate) {
+//       setState(() {
+//         _startDate = picked;
+//       });
+//       await fetchImages(
+//           widget.deviceId, _startDate!); // Fetch images on date selection
+//     }
+//   }
+
+//   Future<void> fetchImages(String device, DateTime date) async {
+//     final formattedDate = DateFormat('dd-MM-yyyy').format(date);
+//     try {
+//       final response = await http.get(Uri.parse(
+//           'https://tulp6xq61c.execute-api.us-east-1.amazonaws.com/dep/images?device=$device&date=$formattedDate'));
+
+//       if (response.statusCode == 200) {
+//         final bodyJson = json.decode(response.body);
+//         final imagesData =
+//             json.decode(bodyJson['body'])['images'] as List<dynamic>;
+
+//         // Create a list to hold the image URL and timestamp pairs
+//         List<Map<String, String>> imageList = [];
+//         for (var imageData in imagesData) {
+//           // Extract URL and timestamp
+//           String url = imageData['url'];
+//           String timestamp = imageData['url'].split('/')[3];
+//           imageList.add({'url': url, 'timestamp': timestamp});
+//         }
+
+//         setState(() {
+//           imageUrls = imageList;
+//         });
+//       } else {
+//         throw Exception('Failed to load images');
+//       }
+//     } catch (e) {
+//       print('Error fetching images: $e');
+//       Fluttertoast.showToast(
+//         msg: 'Error fetching images',
+//         toastLength: Toast.LENGTH_LONG,
+//       );
+//     }
+//   }
+
+//   void _downloadAllImages() {
+//     // Generate text file content with total count, image URLs, and timestamps
+//     int totalCount = imageUrls.length;
+//     StringBuffer buffer = StringBuffer();
+//     buffer.writeln('Total Images: $totalCount');
+//     for (var image in imageUrls) {
+//       buffer.writeln('${image['url']} | ${image['timestamp']}');
+//     }
+
+//     String fileContent = buffer.toString();
+
+//     // Encode content as UTF-8
+//     List<int> encodedContent = utf8.encode(fileContent);
+
+//     // Create blob URL for download
+//     String blobUrl = 'data:application/octet-stream;charset=utf-8;base64,' +
+//         base64Encode(encodedContent);
+
+//     // Create anchor element for download
+//     html.AnchorElement anchorElement = html.AnchorElement(href: blobUrl)
+//       ..setAttribute('download', 'images.txt');
+
+//     // Trigger download
+//     anchorElement.click();
+//   }
+// }
